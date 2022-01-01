aefaf159f2e008090407675805eaf4c3baa30331
Jay Logelin <jay.logelin@gmail.com>
2022-01-01 12:20:06 -0500

    fixed bug with refactored gates code

--- io/io.go
+++ io/io.go
@@ -65,3 +65,32 @@ func ReadPrivKey(filename string) (*gates.PrivateKey, error) {
 	dataFile.Close()
 	return &key, nil
 }
+
+func WriteCiphertext(ctxt *gates.Int, filename string) error {
+	file, _ := os.Create(filename)
+	defer file.Close()
+	encoder := gob.NewEncoder(file)
+	err := encoder.Encode(ctxt)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	return nil
+}
+
+func ReadCiphertext(filename string) (*gates.Int, error) {
+	var ctxt *gates.Int
+	dataFile, err := os.Open(filename)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	dataDecoder := gob.NewDecoder(dataFile)
+	err = dataDecoder.Decode(&ctxt)
+	if err != nil {
+		fmt.Println(err)
+		return nil, err
+	}
+	dataFile.Close()
+	return ctxt, nil
+}
